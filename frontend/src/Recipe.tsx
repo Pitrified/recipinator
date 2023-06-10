be807de4c7b346d6981ca2981b395d594a98f191
Pitrified <nobilipietro@gmail.com>
2023-06-10 21:48:53 +0200

    Add layout for editing steps

--- frontend/src/Recipe.tsx
+++ frontend/src/Recipe.tsx
@@ -5,11 +5,59 @@ import "./Recipe.css";
 const api_url = "http://127.0.0.1:8000";
 
 const Step = ({ id, text, handleAddStepAfter }) => {
+  const [editing, setEditing] = useState(false);
+  const [updatedText, setUpdatedText] = useState(text);
+
+  const handleEditClick = () => {
+    setEditing(true);
+  };
+
+  const handleInputChange = (e) => {
+    setUpdatedText(e.target.value);
+  };
+
+  const handleSaveClick = async () => {
+    try {
+      console.log("Step to save:", updatedText);
+      // const response = await axios.put(`${api_url}/api/step/${id}`, {
+      //   text: updatedText,
+      // });
+      // console.log("Step saved:", response.data);
+      // You can perform any additional actions after the step is successfully saved
+      setEditing(false);
+    } catch (error) {
+      console.error("Error saving step:", error);
+    }
+  };
+
   return (
     <li className="step-item">
-      <span className="step-number">Step {id}:</span>
-      <div className="step-text">{text}</div>
-      <button onClick={handleAddStepAfter}>+</button>
+      {editing ? (
+        <div className="step-item-cont">
+          <input
+            type="text"
+            className="step-text"
+            value={updatedText}
+            onChange={handleInputChange}
+          />
+          <button className="save-step-button" onClick={handleSaveClick}>
+            Save
+          </button>
+        </div>
+      ) : (
+        <div className="step-item-cont">
+          <div className="step-header">
+            <span className="step-number">Step {id}:</span>
+            <p className="step-text">{text}</p>
+          </div>
+          <div className="step-actions">
+            <button className="edit-step-button" onClick={handleEditClick}>
+              Edit
+            </button>
+            <button onClick={handleAddStepAfter}>+</button>
+          </div>
+        </div>
+      )}
     </li>
   );
 };
